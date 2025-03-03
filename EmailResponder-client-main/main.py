6ab31a8f5d0c2abdee43783eb2e182cea9fcd645
NeoTrovelaVillamiel <ntrovelavillamiel@gmail.com>
2025-03-03 11:40:30 -0600

    added some small things to main files

--- EmailResponder-client-main/main.py
+++ EmailResponder-client-main/main.py
@@ -1,1 +1,6 @@
-# empty python file
+# client side python app for AIEmailResponder
+# work with web service
+
+import requests  # calling web service
+import jsons  # relational-object mapping
+
--- EmailResponder-server-main/app.js
+++ EmailResponder-server-main/app.js
@@ -1,1 +1,5 @@
-// empty js file
+// express js and node.js web service, interacts with 
+// AWS S3 and RDS
+
+const express = require('express');
+const app = express();
